42cea56c024042d2f1f2f893897974a7a494a155
Prajwal <prajwalgbdr03@gmail.com>
2023-07-14 20:14:51 +0530

    train and evaluate stage completed

---
+++ src/stage06_train_and_evaluate.py
@@ -0,0 +1,122 @@
+import os
+import yaml
+import sys
+import pandas as pd
+import numpy as np
+import argparse
+import joblib
+import json
+
+from stage01_get_data import read_params
+from application_logging import logging
+from app_exception.app_exception import CustomException
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+
+
+def train_and_evaluate(config_path):
+    
+    config = read_params(config_path)
+
+    train_data_path = config["split_data"]["train_path"]
+    test_data_path = config["split_data"]["test_path"]
+    random_state = config["base"]["random_state"]
+    model_dir = config["model_dirs"]
+    target = config["base"]["target_col"]
+
+
+    train = pd.read_csv(train_data_path,sep=",")
+    test = pd.read_csv(test_data_path,sep=',')
+
+    train_y = train[target]
+    test_y = test[target]
+    
+    train_X = train.drop(target,axis=1)
+    test_X = test.drop(target, axis=1)
+
+
+    # Hyper parameter tuning
+    n_estimators = config["estimators"]["RandomForestClassifier"]["params"]["n_estimators"]
+    min_samples_split = config["estimators"]["RandomForestClassifier"]["params"]["min_samples_split"]
+    min_samples_leaf = config["estimators"]["RandomForestClassifier"]["params"]["min_samples_leaf"]
+    max_features = config["estimators"]["RandomForestClassifier"]["params"]["max_features"]
+    max_depth = config["estimators"]["RandomForestClassifier"]["params"]["max_depth"]
+    criterion = config["estimators"]["RandomForestClassifier"]["params"]["criterion"]
+    
+    
+    
+    rf_clf = RandomForestClassifier(
+                                    n_estimators=n_estimators,
+                                    min_samples_split=min_samples_split,
+                                    min_samples_leaf=min_samples_leaf,
+                                    max_features = max_features,
+                                    max_depth=max_depth,
+                                    criterion=criterion
+                                    )
+    
+    rf_clf.fit(train_X, train_y)
+
+
+    y_pred = rf_clf.predict(test_X)
+    
+    accuracy = accuracy_score(test_y,y_pred)
+    print(accuracy)
+    clf_report = classification_report(test_y,y_pred)
+    print(clf_report)
+
+
+    scores_file = config["reports"]["scores"]
+    params_file = config['reports']['params']
+
+    with open(scores_file, 'w') as f:
+        scores = {
+            'accuracy': accuracy,
+            'classification_report': clf_report
+        }
+
+        json.dump(scores, f, indent=4)
+
+
+    with open(params_file, 'w') as f:
+        params = {
+            "n_estimators":n_estimators,
+            "min_samples_split":min_samples_split,
+            "min_samples_leaf": min_samples_leaf,
+            "max_features": max_features,
+            "max_depth": max_depth,
+            "criterion": criterion
+            
+        }
+        json.dump(params, f, indent=4)
+
+
+
+
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir,"model.joblib")
+    joblib.dump(rf_clf, model_path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument('--config', default='params.yaml')
+    parsed_args = args.parse_args()
+    train_and_evaluate(config_path=parsed_args.config)
